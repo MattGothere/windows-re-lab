e33753d49bb79cc546e11d38b06cb64847259732
Ma_t__T <233060950+MattGothere@users.noreply.github.com>
2026-08-19 16:09:51 +0800

    refactor: reorganize process investigation project

--- ProcessInspector/ProcessInspector.cpp
+++ ProcessInspector/ProcessInspector.cpp
@@ -7,7 +7,7 @@ int main()
     HANDLE snap; // create a handle, helping us to access proccess information                        
     bool handle_closeing;
 
-    snap = CreateToolhelp32Snapshot(    
+    snap = CreateToolhelp32Snapshot(
         TH32CS_SNAPPROCESS,             // include all process in the system
         0                               // current process
     );
@@ -20,7 +20,7 @@ int main()
     PROCESSENTRY32 pe;                    // open up a "sheet" to let functions fill in   
     pe.dwSize = sizeof(pe);    // initialize dwsize
 
-    bool first;                             
+    bool first;
     first = Process32First(                 // fill up the very first sheet
         snap,
         &pe
@@ -33,18 +33,18 @@ int main()
     }
 
     // ***全部改用寬字元串流***
-    std::wcout << L"process name: " << pe.szExeFile 
-               << L" PID: " << pe.th32ProcessID 
-               << L" Threads: " << pe.cntThreads 
-               << std::endl;
-
-
-    while (Process32Next(snap, &pe)) {                 // keep listing if there's still next
-    std::wcout
-        << L"process name: " << pe.szExeFile
+    std::wcout << L"process name: " << pe.szExeFile
         << L" PID: " << pe.th32ProcessID
         << L" Threads: " << pe.cntThreads
         << std::endl;
+
+
+    while (Process32Next(snap, &pe)) {                 // keep listing if there's still next
+        std::wcout
+            << L"process name: " << pe.szExeFile
+            << L" PID: " << pe.th32ProcessID
+            << L" Threads: " << pe.cntThreads
+            << std::endl;
     }
 
     if (GetLastError() != ERROR_NO_MORE_FILES) {
